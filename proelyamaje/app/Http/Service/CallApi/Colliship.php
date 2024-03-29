8de97ff6572039f5b5e854678a35d5f263484509
martial <martial@elyamaje.com>
2024-03-29 14:57:12 +0100

    cron colliship

--- proelyamaje/app/Http/Service/CallApi/Colliship.php
+++ proelyamaje/app/Http/Service/CallApi/Colliship.php
@@ -43,9 +43,8 @@ class Colliship
             // recupérer ...
              $ids_f = implode(',',$ids_fact);
 
-             dd($ids_f);
              
-             DB::connection('mysql2')->select("UPDATE llxyq_facture_extrafields SET col=1 WHERE fk_object IN (".$ids_f.")");
+             DB::connection('mysql2')->select("UPDATE llxyq_facture_extrafields SET col=1 WHERE fk_object IN ('.$ids_f.')");
              
              dd('succes');
      }
