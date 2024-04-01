500ce1be1fe883c579ec14c4c6c6b48b5a8e9d86
martial <martial@elyamaje.com>
2024-04-01 11:37:43 +0200

    test function update

--- proelyamaje/app/Http/Service/CallApi/Colliship.php
+++ proelyamaje/app/Http/Service/CallApi/Colliship.php
@@ -53,16 +53,15 @@ class Colliship
                     foreach($data as $valu){
                     $ids_fact[] = $valu['rowid'];
                  }
-               // recupérer les fk_facture
-               // faire un update dans la table llxyq_facture_extrafields
+                // recupérer les fk_facture
+                // faire un update dans la table llxyq_facture_extrafields
                 // recupérer ...
-
-               // recupérer la difference des 
+                 // recupérer la difference des 
               $resut_data_diff = array_diff($ids_fact,$result_data);
 
               dd($result_data_diff);
-             // faire des insert ici pour .
-              // faire un insert d'ecriture de paiement facture du montant en espéce.
+               // faire des insert ici pour .
+               // faire un insert d'ecriture de paiement facture du montant en espéce.
               foreach($result_data_diff as $vl){
                   DB::connection('mysql2')->table(' llxyq_facture_extrafields')->insert([
                    'tms' => date('Y-m-d H:i:s'),
