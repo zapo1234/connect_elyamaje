d8f258315189328bf0b1d3bf0326c25f64ca89d0
martial <martial@elyamaje.com>
2024-04-01 10:14:13 +0200

    function cron colliship

--- proelyamaje/app/Http/Service/CallApi/Colliship.php
+++ proelyamaje/app/Http/Service/CallApi/Colliship.php
@@ -28,32 +28,27 @@ class Colliship
                 // Soustrait un jour à la date actuelle
                 $date->modify('-5 day');
                 $date_true = $date->format('Y-m-d');
-               $array_montant =  array('59.60','55.62','55.60');
-               $array_montants = implode(',',$array_montant);
-               $datas_facture = DB::connection('mysql2')->select("SELECT rowid ,datec FROM llxyq_facture WHERE total_ttc IN (".$array_montants.") AND  datef > '$date_true' AND fk_mode_reglement=54  AND paye=1");
-               $datas = json_encode($datas_facture);
-               $data = json_decode($datas,true);
-               $ids_fact =[];
+                $array_montant =  array('59.60','55.62','55.60');
+                $array_montants = implode(',',$array_montant);
+                $datas_facture = DB::connection('mysql2')->select("SELECT rowid ,datec FROM llxyq_facture WHERE total_ttc IN (".$array_montants.") AND  datef > '$date_true' AND fk_mode_reglement=54  AND paye=1");
+                $datas = json_encode($datas_facture);
+                $data = json_decode($datas,true);
+                $ids_fact =[];
 
-            
-              foreach($data as $valu){
-                 $ids_fact[] = $valu['rowid'];
-             }
-             // recupérer les fk_facture
-             // faire un update dans la table llxyq_facture_extrafields
-             // recupérer ...
-             $ids_f = implode(',',$ids_fact);
-
-               DB::enableQueryLog();
-               DB::connection('mysql2')->select("UPDATE llxyq_facture_extrafields SET col=1 WHERE fk_object IN ('.$ids_f.')");
+                foreach($data as $valu){
+                  $ids_fact[] = $valu['rowid'];
+                }
+                // recupérer les fk_facture
+                // faire un update dans la table llxyq_facture_extrafields
+                // recupérer ...
+                 $ids_f = implode(',',$ids_fact);
+                 $response = DB::connection('mysql2')->select("UPDATE llxyq_facture_extrafields SET col=1 WHERE fk_object IN ('.$ids_f.')");
                //$reponse = DB::connection('mysql2')->update("UPDATE llxyq_facture_extrafields SET col=1 WHERE fk_object IN ('.$ids_f.')");
-               dd(DB::getQueryLog());
-               dd('reponse_true');
-          
-           } catch (Exception $e) {
-            // Capture de l'erreur et affichage du message
-            dd($e->getMessage());
-            echo "Une erreur s'est produite : " . $e->getMessage();
+                var_dump($response);
+             }  catch (\Illuminate\Database\QueryException $e) {
+              // Capture de l'erreur et affichage du message
+               dd($e->getMessage());
+              echo "Une erreur s'est produite : " . $e->getMessage();
        }
       
     }
