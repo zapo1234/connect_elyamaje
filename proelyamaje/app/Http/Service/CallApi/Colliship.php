17068669c6d3ba564f055d9db2b9acdf50f034a7
martial <martial@elyamaje.com>
2024-04-01 10:37:14 +0200

    test na table

--- proelyamaje/app/Http/Service/CallApi/Colliship.php
+++ proelyamaje/app/Http/Service/CallApi/Colliship.php
@@ -36,9 +36,9 @@ class Colliship
                 $ids_fact =[];
 
                 // faire un select 
-                $datas_factures = DB::connection('mysql2')->select("SELECT rowid ,datec FROM lxyq_facture_extrafields");
+                $datas_factures = DB::connection('mysql2')->select("SELECT rowid ,datec FROM llxyq_facture_extrafields");
                 $json = json_encode($datas_factures);
-                $json_true = json_decode($datas,true);
+                $json_true = json_decode($json,true);
                 
                 dump($data);
                 dump($json_true);
