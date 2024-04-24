e76431f6d12f3fa8c98add6f6f92b1a402da3b02
martial <martial@elyamaje.com>
2024-04-24 09:31:21 +0200

    function test

--- proelyamaje/resources/views/email/codeenvoi.blade.php
+++ proelyamaje/resources/views/email/codeenvoi.blade.php
@@ -12,7 +12,7 @@
                         <a href="{{  route('ambassadrice.user')   }}">
                             <img i src="{{ asset('admin/img/Logo_elyamaje.png')}}" width="95px"; height="auto"; style="margin-top:20px;";>
                         </a>       
-                        L'Ambassadrice Elyamaje vous a attribué un code promotionnel. Grâce à ce code, vous avez accès à 10% de reduction sur tous les produits du site <a href="elyamaje.com">elyamaje.com</a>.<br/><br/>
+                        L'Ambassadrice Elyamaje vous a attribué un code promotionnel. Grâce à ce code, vous avez accès à 10% de reduction sur tous les produits du site <a href="https://www.elyamaje.com">elyamaje.com</a>.<br/><br/>
                       Voici votre code élève <strong>{{ $code }}</strong> pour effectuer vos achats. <br><br>
                       Ce code est valable une seule fois lors de votre première commande.<br><br>
                     </td>
