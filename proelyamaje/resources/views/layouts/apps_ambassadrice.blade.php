68355df2fe45789a2fd8973b12187382e0566af8
martial <martial@elyamaje.com>
2024-05-16 12:20:03 +0200

    supression loader

--- proelyamaje/resources/views/layouts/apps_ambassadrice.blade.php
+++ proelyamaje/resources/views/layouts/apps_ambassadrice.blade.php
@@ -12,9 +12,6 @@
     <link href="{{ asset('admin/assets/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
 	<link href="{{ asset('admin/assets/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
 	<link href="{{ asset('admin/assets/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
-	<!-- loader-->
-	<link href="{{ asset('admin/assets/assets/css/pace.min.css') }}" rel="stylesheet" />
-	<script src="{{ asset('admin/assets/assets/js/pace.min.js') }}"></script>
 	<!-- Bootstrap CSS -->
 	<link href="{{ asset('admin/assets/assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('admin/assets/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
