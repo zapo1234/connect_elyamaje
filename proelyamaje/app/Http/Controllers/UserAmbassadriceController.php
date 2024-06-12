422ea2e1abf404de0fd547ec567db03db7835d8a
martial <martial@elyamaje.com>
2024-06-12 10:32:19 +0200

    voir function

--- proelyamaje/app/Http/Controllers/UserAmbassadriceController.php
+++ proelyamaje/app/Http/Controllers/UserAmbassadriceController.php
@@ -140,8 +140,12 @@ class UserAmbassadriceController
             
                 ];
             }
+
+            dd($result_data);
+            
            }
-           }
+
+          }
           }
          return view('utilisateurs.statsdatauser',['result_data'=>$result_data,'result_datas'=>$result_datas,'result_name'=>$result_name]);
     }
