721e50c2be421feb47318d090e10a3c590449cd7
martial <martial@elyamaje.com>
2024-07-24 15:45:57 +0200

    ps

--- proelyamaje/resources/views/api/confirmtransferorder.blade.php
+++ proelyamaje/resources/views/api/confirmtransferorder.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <!-- Custom fonts for this template-->
+     <!-- Custom fonts for this template--->
     <link href="https://prodev.elyamaje.com/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link rel="icon" type="image/png" href="https://prodev.elyamaje.com/admin/img/Logo_elyamaje.png" />
     <link
